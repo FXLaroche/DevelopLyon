3217840fcadb04334e60f84b9945be47849f3301
MatthieuChabot <chbt.matthieu@gmail.com>
2021-11-04 21:46:44 +0100

    adding bootstrap elements and css on user, show, edit, index and header pages

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -23,7 +23,7 @@ class UserController extends AbstractController
             } else {
                 $userManager = new UserManager();
                 $userManager->registerUser($user);
-                header('Location:/user/login');
+                header('Location: /user/login');
             }
         }
 
@@ -66,24 +66,6 @@ class UserController extends AbstractController
             $userManager = new UserManager();
             $userManager->delete((int)$id);
             header('Location:/users');
-        }
-    }
-
-    public function connect($user)
-    {
-
-        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-            $login = array_map('trim', $_POST);
-            if (isset($login['email']) && $login['password']) {
-                if (password_verify($user['password'], $login['password'])) {
-                    $userManager = new UserManager();
-                    $userManager->login($login);
-                    header('Location: /users');
-                } else {
-                    echo "Le mot de passe n'est pas le bon";
-                }
-            }
-            return $this->twig->render('User/login.html.twig');
         }
     }
 }
--- src/Model/UserManager.php
+++ src/Model/UserManager.php
@@ -12,8 +12,7 @@ class UserManager extends AbstractManager
         VALUES (:nickname, :email, :password)");
         $statement->bindValue(':nickname', $user['nickname'], \PDO::PARAM_STR);
         $statement->bindValue(':email', $user['email'], \PDO::PARAM_STR);
-        // $statement->bindValue(':password', password_hash($user['password'], PASSWORD_BCRYPT), \PDO::PARAM_STR);
-        $statement->bindValue(':password', $user['password'], \PDO::PARAM_STR);
+        $statement->bindValue(':password', password_hash($user['password'], PASSWORD_BCRYPT), \PDO::PARAM_STR);
         $statement->execute();
         return (int)$this->pdo->lastInsertId();
     }
@@ -23,18 +22,9 @@ class UserManager extends AbstractManager
         $statement = $this->pdo->prepare("UPDATE " . self::TABLE . " SET 
         nickname = :nickname AND password = :password AND email = :email WHERE id=:id");
         $statement->bindValue(':nickname', $user['nickname'], \PDO::PARAM_STR);
-        $statement->bindValue(':password', $user['password'], \PDO::PARAM_STR);
+        $statement->bindValue(':password', password_hash($user['password'], PASSWORD_BCRYPT), \PDO::PARAM_STR);
         $statement->bindValue(':email', $user['email'], \PDO::PARAM_STR);
         $statement->bindValue(':id', $user['id'], \PDO::PARAM_INT);
-
-        return $statement->execute();
-    }
-
-    public function login(array $user): bool
-    {
-        $statement = $this->pdo->prepare('SELECT * FROM user WHERE email=:email AND password=:password');
-        $statement->bindValue(':email', $user['email'], \PDO::PARAM_STR);
-        $statement->bindValue(':password', $user['password'], \PDO::PARAM_STR);
 
         return $statement->execute();
     }
